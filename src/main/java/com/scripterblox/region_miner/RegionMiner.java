0b11a0f94d01ad5d8e54cb1f7d0ee0190a70bf23
SCRIPTERBLOX <scriptoblox@gmail.com>
2025-02-18 11:02:31 +0000

    Initial Commit

---
+++ src/main/java/com/scripterblox/region_miner/RegionMiner.java
@@ -0,0 +1,16 @@
+package com.scripterblox.region_miner;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RegionMiner implements ModInitializer {
+	public static final String MOD_ID = "region_miner";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/scripterblox/region_miner/RegionMinerClient.java
@@ -0,0 +1,10 @@
+package com.scripterblox.region_miner;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RegionMinerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
